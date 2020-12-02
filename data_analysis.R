c7861485c4ead8542e75aee913bc49f9c72d631a
dscanferla <damianox85@gmail.com>
2020-12-02 17:34:03 +0200

    Analysis: add boxplot

--- data_analysis.R
+++ data_analysis.R
@@ -57,7 +57,7 @@ data.woreda <- data.woreda[!sapply(data.woreda, is.null)] %>% reduce(full_join, 
 write.xlsx(data.woreda, "data.woreda.xlsx")
 
 ##########################################################################################################
-# Step 3: determine list of indicators
+# Step 3: generate analysis output for InDesign
 ##########################################################################################################
 
 analysis.national <- run.analysis(data.woreda, "adm0_national")
@@ -65,3 +65,49 @@ analysis.region <- run.analysis(data.woreda, "adm1_region")
 analysis.zone <- run.analysis(data.woreda, "adm2_zone")
 analysis <- rbind(analysis.national, analysis.region, analysis.zone)
 write.xlsx(analysis, "analysis.xlsx")
+
+##########################################################################################################
+# Step 4: generate boxplot at national level
+##########################################################################################################
+
+labels <- read_excel("resources/item_labels.xlsx")
+cols <- colnames(data.woreda)[str_detect(colnames(data.woreda), "price_per_unit")]
+data <- data.woreda[cols] %>% 
+  pivot_longer(cols=all_of(cols), names_to="item", values_to="price_per_unit") %>% 
+  filter(!is.na(price_per_unit)) %>% left_join(labels, by="item") %>% 
+  mutate(item=paste0(label, "\n(", unit, ")")) %>% select(-c("label", "unit"))
+
+boxplot_statistics <- function(x) {
+  r <- quantile(x, probs = c(0.00, 0.25, 0.5, 0.75, 1))
+  names(r) <- c("ymin", "lower", "middle", "upper", "ymax")
+  return(r)
+}
+
+get.number.label <- function(item, value){
+  rounding <- ifelse(str_starts(item, "Water"), 2, 0)
+  df <- data.frame(value=value, rounding=rounding)
+  return(apply(df, 1, function(x) return(format(round(x["value"], digits=x["rounding"]), nsmall=0))))
+}
+
+medians <- plyr::ddply(data, "item", summarise, med = median(price_per_unit, na.rm=T))
+mins <- plyr::ddply(data, "item", summarise, min = min(price_per_unit, na.rm=T))
+maxs <- plyr::ddply(data, "item", summarise, max = max(price_per_unit, na.rm=T))
+
+ggplot(data,aes(item, price_per_unit, width=0.3)) +
+  stat_summary(fun.data = boxplot_statistics, geom="boxplot", fill = "#D1D3D4") +
+  theme_bw() + 
+  geom_text(data=mins, 
+            aes(x=item, y=min, label=get.number.label(item, min)),
+            size=2.5, vjust=1.5) +
+  geom_text(data=medians, 
+            aes(x=item, y=med, label=get.number.label(item, med)),
+            size=2.5, hjust = -1) +
+  geom_text(data=maxs, 
+            aes(x=item, y=max, label=get.number.label(item, max)),
+            size=2.5, vjust =-0.5) +
+  theme(axis.text.x = element_text(angle = 0, size = 7, hjust = 0.5 ),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  ggsave("boxplot.pdf", width=27, height=12, units="cm", device="pdf")
+  
+
