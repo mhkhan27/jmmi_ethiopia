db937bddc9d7b7aa79f62adc516294e5c7d4d796
Md. Mehedi Hasan Khan <mhkhan27@users.noreply.github.com>
2020-12-14 16:57:48 +0600

    no_change

--- config.R
+++ config.R
@@ -7,7 +7,7 @@ source("./utils.R")
 # PLEASE CHECK THE FOLLOWING PARAMETERS AT EACH ROUND AND UPDATE IF NEEDED
 ##########################################################################################################
 # specify month of the assessment (it is used in the name of the output files)
-assessment.month <- "2020-11"
+assessment.month <- "2020-12"
 # specify URLs and input filenames
 filename.tool <- "resources/ETH_JMMI_Kobo_v2.xlsx"
 filename.email.list <- "resources/2020-07-15_email_list.xlsx"
